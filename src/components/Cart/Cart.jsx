d19aa5aab7f4404d86e4e4a56c5cfdc8fe46c8f7
spkumerjoy <joyskydd@gmail.com>
2023-03-29 01:27:10 +0600

    Show all data from cart-successfully

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,19 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice += product.price;
+        // product.quantity = product.quantity || 1;
+        totalPrice += product.price * product.quantity;
         totalShipping += product.shipping;
+        quantity += product.quantity;
     }
     const tax = (totalPrice * 7) / 100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className="cart">
             <h2>Order Summary</h2>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -32,7 +32,19 @@ const Shop = () => {
         setCart(savedCart);
     }, [products]);
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        let newCart = [];
+        //if product doesn't exist in the cart,then set quantity = 1
+        //if exist update quantity by 1
+        const exists = cart.find((pd) => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        } else {
+            exists.quantity += 1;
+            const remaining = cart.filter((pd) => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+        // const newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id);
     };
